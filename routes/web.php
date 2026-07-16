b8283bc71b3e4f956fde316208e69fbe01c8b152
vanh26hihi <vietanhdoan987@gmail.com>
2026-07-16 22:14:25 +0700

    Thêm route quản trị phim

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\Admin\CinemaController as AdminCinemaController;
+use App\Http\Controllers\Admin\GenreController as AdminGenreController;
+use App\Http\Controllers\Admin\MovieController as AdminMovieController;
+use App\Http\Controllers\Admin\RoomController as AdminRoomController;
+use App\Http\Controllers\Admin\SeatController as AdminSeatController;
+use App\Http\Controllers\Admin\ShowtimeController as AdminShowtimeController;
+use App\Http\Controllers\User\BookingController;
+use App\Http\Controllers\User\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -13,13 +21,9 @@ Route::get('/register', function () {
     return view('user.auth.register');
 })->name('register');
 
-Route::get('/movies', function () {
-    return view('user.movies.index');
-})->name('user.movies.index');
+Route::get('/movies', [MovieController::class, 'index'])->name('user.movies.index');
 
-Route::get('/movies/{id}', function ($id) {
-    return view('user.movies.show');
-})->name('user.movies.show');
+Route::get('/movies/{slug}', [MovieController::class, 'show'])->name('user.movies.show');
 
 Route::get('/booking/select-seat', function () {
     return view('user.bookings.select-seat');
@@ -52,3 +56,21 @@ Route::get('/ai/chatbot', function () {
 Route::get('/profile', function () {
     return view('user.profile.index');
 })->name('user.profile');
+
+Route::get('/api/cinemas/{cinema}/rooms', function (App\Models\Cinema $cinema) {
+    return $cinema->rooms()->select('id', 'name')->get();
+})->name('api.cinemas.rooms');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('movies', AdminMovieController::class);
+    Route::resource('genres', AdminGenreController::class)->except(['show']);
+    Route::resource('cinemas', AdminCinemaController::class)->except(['show']);
+    Route::resource('rooms', AdminRoomController::class)->except(['show']);
+
+    Route::get('/seats', [AdminSeatController::class, 'index'])->name('seats.index');
+    Route::get('/seats/manage/{room}', [AdminSeatController::class, 'manage'])->name('seats.manage');
+    Route::post('/seats/generate/{room}', [AdminSeatController::class, 'generate'])->name('seats.generate');
+    Route::patch('/seats/{seat}', [AdminSeatController::class, 'update'])->name('seats.update');
+
+    Route::resource('showtimes', AdminShowtimeController::class)->except(['show']);
+});
