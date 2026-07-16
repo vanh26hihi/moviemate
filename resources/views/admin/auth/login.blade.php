c925b938c3a6e038e7fe7962fd622d4b6dd00095
daohongduc9-rgb <daohongduc9@gmail.com>
2026-07-16 18:12:26 +0700

    LOGIN-1

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Login - MovieMate</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
 </head>
 <body>
     
