187d08b825b757325bcf6184463779fe781d811a
Sang <sangvlog12345@gmail.com>
2026-07-17 14:13:05 +0700

    trang hiển thị món ăn

---
+++ app/Http/Controllers/User/OrderController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\FoodItem;
+use App\Models\Order;
+use App\Models\OrderItem;
+
+class OrderController extends Controller
+{
+}
