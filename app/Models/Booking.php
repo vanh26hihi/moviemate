19aefd47956a3e281b485065f1d73a449be01fe5
truongblua06 <truongdtry06@gmail.com>
2026-07-21 09:20:57 +0700

    update models booking, ticket, payment

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -304,4 +304,32 @@ class Booking extends Model
         return $this->hasMany(Ticket::class);
     }
 }
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BookingSeat extends Model
+{
+    protected $fillable = [
+        'booking_id',
+        'seat_id',
+        'price'
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2'
+    ];
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+
+    public function seat()
+    {
+        return $this->belongsTo(Seat::class);
+    }
+}
 }
